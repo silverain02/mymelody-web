6429212d8406712078d8a09d385ba819eb7021a6
silver <ekdldkaa@naver.com>
2024-11-08 12:22:34 +0900

    fix : trailing slash추가

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,7 +4,3 @@ export const BASE_URL = process.env.NEXT_PUBLIC_FE_URL;
 // Spotify 관련 URL
 export const SPOTIFY_AUTHORIZE_URL = 'https://accounts.spotify.com/authorize';
 export const SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token';
-
-// API 엔드포인트 URL
-export const API_BASE_URL = `${BASE_URL}/api`;
-export const API_CALLBACK_URL = `${API_BASE_URL}/callback`;
