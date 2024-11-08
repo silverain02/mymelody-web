f81cc672758b8ac82e04220aaef75a8d21a409dc
silver <ekdldkaa@naver.com>
2024-11-08 11:31:19 +0900

    fix : query추출 함수 보정

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,12 +1,17 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
 /* eslint-disable react/function-component-definition */
-import { NextApiRequest, NextApiResponse } from 'next';
+export default function Page() {
+  const router = useRouter();
+  const [queryParams, setQueryParams] = useState({});
 
-export default function Page(req: NextApiRequest, res: NextApiResponse) {
-  const code = typeof req.query.code === 'string' ? req.query.code : null;
-  const state = typeof req.query.state === 'string' ? req.query.state : null;
-
-  console.log(code);
-  console.log(state);
+  useEffect(() => {
+    if (router.isReady) {
+      setQueryParams(router.query);
+      console.log(JSON.stringify(queryParams, null, 2));
+    }
+  }, [router.isReady, router.query]);
 
   return (
     <>
