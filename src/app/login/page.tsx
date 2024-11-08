dcb4d8640c13e6bc13b9cbac3659d086dd3cfeb5
silver <ekdldkaa@naver.com>
2024-11-08 12:41:14 +0900

    fix : redirect uir 엔코딩 전환

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,12 +4,6 @@ import { redirect } from 'next/navigation';
 
 export default function page() {
   redirect(
-    SPOTIFY_AUTHORIZE_URL +
-      '?' +
-      qs.stringify({
-        response_type: 'code',
-        client_id: process.env.NEXT_PUBLIC_SPOTIFY_BE_CLIENT_ID,
-        redirect_uri: `${BASE_URL}/callback`,
-      })
+    `${SPOTIFY_AUTHORIZE_URL}?response_type=code&client_id=${process.env.NEXT_PUBLIC_SPOTIFY_BE_CLIENT_ID}&redirect_uri=${BASE_URL}/callback`
   );
 }
