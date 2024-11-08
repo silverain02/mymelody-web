b5a5f11328010e7c24a707597beb74cc4baf9995
silver <ekdldkaa@naver.com>
2024-11-08 13:47:31 +0900

    feat : spotify login callback 쿼리 추출

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,16 +1,15 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
 
 /* eslint-disable react/function-component-definition */
 export default function Page() {
   const searchParams = useSearchParams();
-  console.log(searchParams);
+  const code = searchParams.get('code');
 
   return (
     <>
-      <h1>this is callback</h1>
+      <h1>code</h1>
     </>
   );
 }
