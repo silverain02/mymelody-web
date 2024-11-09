323a1133f0853ff821e684dc62ca2c819e6b2968
silver <ekdldkaa@naver.com>
2024-11-09 17:51:44 +0900

    feat : userToken전역변수화

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -10,18 +10,16 @@ export default function Page() {
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
 
-  console.log(code);
-
   //userToken 받아오기
   const { userTokenObj, error } = useGetUserToken(code);
   //userToken 전역변수 저장
-  // const { userToken, setUserToken } = useUserTokenStore();
+  const { userToken, setUserToken } = useUserTokenStore();
 
   useEffect(() => {
     if (userTokenObj) {
       console.log(userTokenObj);
-      // setUserToken(userAccessToken, userRefreshToken);
-      // console.log(userToken?.accessToken);
+      setUserToken(userTokenObj.accessToken, userTokenObj.refreshToken);
+      console.log(userToken?.accessToken);
     }
 
     if (error) {
