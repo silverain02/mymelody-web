0be91c5af2825243c4a74fd738c112455e6f3971
silver <ekdldkaa@naver.com>
2024-11-12 22:04:55 +0900

    minW지정

--- src/components/TrackModule.tsx
+++ src/components/TrackModule.tsx
@@ -78,7 +78,7 @@ const TrackModule: React.FC<TrackModuleProps> = ({ isrc, onTrackClick }) => {
       borderRadius="lg"
       boxShadow="md"
       cursor="pointer"
-      maxW="300px"
+      minW="15rem"
     >
       {/* 앨범 이미지 */}
       <Box position="relative" onClick={handleAlbumClick}>
