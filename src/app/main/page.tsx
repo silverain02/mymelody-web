761393f70d6f965226ccf1354b871775068b58e6
silver <ekdldkaa@naver.com>
2024-11-12 18:54:40 +0900

    chore : selectBar분리

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,58 +1,20 @@
-import { useGetMelodyFiltered } from '@/apis/api/get/useGetMelodyFiltered';
+import { FilterSelectBar } from '@/components/FilterSelectBar';
 import KakaoMap from '@/components/Map/KakaoMap';
 import useLocationInfo from '@/hooks/useLocationInfo';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Box, Center, Select } from '@chakra-ui/react';
-import { useState, ChangeEvent, useEffect } from 'react';
+import { Box } from '@chakra-ui/react';
 
 // eslint-disable-next-line react/function-component-definition
 export default function Page() {
   const { locationInfo, setLocationInfo, updateLocationInfo } =
     useLocationInfo();
 
-  // State for storing selected filter
-  const [filter, setFilter] = useState<'all' | 'likes' | 'created' | 'comment'>(
-    'all'
-  );
-
-  // Handle filter change
-  const handleFilterChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setFilter(event.target.value as 'all' | 'likes' | 'created' | 'comment');
-  };
-
-  useEffect(() => {
-    console.log(filter);
-  }, [filter]);
-
   return (
     <>
       <ChakraProvider>
         <Box className="relative w-screen h-screen">
           <KakaoMap />
-          {/* 필터링 선택 바 */}
-          <Center
-            className="absolute top-5 left-1/2 transform -translate-x-1/2"
-            top="5vh"
-            zIndex="10"
-          >
-            <Select
-              value={filter} // Sync with state
-              onChange={handleFilterChange} // Handle change
-              size="sm"
-              bg="whiteAlpha.800" // 투명하게 설정
-              color="black"
-              borderRadius="md"
-              shadow="sm"
-              maxWidth="400px"
-              width="50vw" // 원하는 크기로 설정
-              _hover={{ bg: 'whiteAlpha.900' }}
-            >
-              <option value="all">모든 멜로디</option>
-              <option value="created">내가 생성한 멜로디</option>
-              <option value="liked">내가 좋아한 멜로디</option>
-              <option value="reacted">내가 반응한 멜로디</option>
-            </Select>
-          </Center>
+          <FilterSelectBar />
         </Box>
       </ChakraProvider>
     </>
---
+++ src/components/FilterSelectBar.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { Center, Select } from '@chakra-ui/react';
+import { useState, ChangeEvent, useEffect } from 'react';
+
+export const FilterSelectBar = () => {
+  // State for storing selected filter
+  const [filter, setFilter] = useState<'all' | 'likes' | 'created' | 'comment'>(
+    'all'
+  );
+
+  // Handle filter change
+  const handleFilterChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setFilter(event.target.value as 'all' | 'likes' | 'created' | 'comment');
+  };
+
+  useEffect(() => {
+    console.log(filter);
+  }, [filter]);
+
+  return (
+    <>
+      <Center
+        className="absolute top-5 left-1/2 transform -translate-x-1/2"
+        top="5vh"
+        zIndex="10"
+      >
+        <Select
+          value={filter} // Sync with state
+          onChange={handleFilterChange} // Handle change
+          size="sm"
+          bg="whiteAlpha.800" // 투명하게 설정
+          color="black"
+          borderRadius="md"
+          shadow="sm"
+          maxWidth="400px"
+          width="50vw" // 원하는 크기로 설정
+          _hover={{ bg: 'whiteAlpha.900' }}
+        >
+          <option value="all">모든 멜로디</option>
+          <option value="created">내가 생성한 멜로디</option>
+          <option value="liked">내가 좋아한 멜로디</option>
+          <option value="reacted">내가 반응한 멜로디</option>
+        </Select>
+      </Center>
+    </>
+  );
+};
