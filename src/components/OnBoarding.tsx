bd9fe33dee814a176f92f0777ac8e967715d1c3b
silver <ekdldkaa@naver.com>
2024-11-13 16:19:21 +0900

    현위치 마커 삭제

--- src/components/OnBoarding.tsx
+++ src/components/OnBoarding.tsx
@@ -28,7 +28,7 @@ export const OnBoarding = () => {
       <Box width="100%" p={4} pb={8}>
         <Button
           width="100%"
-          colorScheme="pink"
+          colorScheme="#FF003D"
           size="lg"
           onClick={() => {
             router.push('/login');
